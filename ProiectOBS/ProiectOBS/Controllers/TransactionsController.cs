5d91f16a0ef50996915b999c1ea7ee76dc875f75
Mihail-Popescu <mihaixz4@gmail.com>
2023-05-10 21:59:51 +0300

    Committ UltimateFix

---
+++ ProiectOBS/ProiectOBS/Controllers/TransactionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProiectOBS.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult Transactions()
+        {
+            return View();
+        }
+    }
+}
